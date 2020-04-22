c0ebb446a6cd108b8cb5b99aefc9718c9b0f5819
Hugh Alessi <hughalessi@gmail.com>
2020-04-22 12:33:06 -0600

    reorg

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ from simulation import Simulation
 import argparse
 
 def main():
+    simulation = Simulation(number_people = NUMBER_OF_PEOPLE, 
+                            step_size = STEP_SIZE,
+                            infected_range = INFECTED_RANGE)
+    simulation.run(number_days = 10)
+
+    args = parser.parse_args()
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--E', '--exposed', help = 'initial # people exposed', type = int, default =  1)
@@ -22,12 +30,6 @@ def main():
     parser.add_argument('--prob', '--prob_people', help = 'beta knot= probability of infection if meeting an infected person', type = int, default =  .1)
     parser.add_argument('--numb', '--numb_people', help = 'k = total number of people encountered', type = int, default =  10)
 
-    simulation = Simulation(number_people = NUMBER_OF_PEOPLE, 
-                            step_size = STEP_SIZE,
-                            infected_range = INFECTED_RANGE)
-    simulation.run(number_days = 10)
-
     args = parser.parse_args()
-
-if __name__ == "__main__":
-    main()
+    print(args)
+    
